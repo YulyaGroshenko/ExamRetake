55688c1c27fa5a740e11b17ab565d9bb647bce99
Yulia Groshenko <yulyagroshenko@mail.ru>
2021-03-25 23:33:59 +0500

    Готовая игра

---
+++ Gallows/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Gallows
+{
+    class Program
+    {
+        static void Main()
+        {
+            Game.FiilLetters();
+            while (Game.Lifes != 0 && Game.Field.Contains("_ "))
+            {
+                Console.SetCursorPosition(0, 0);
+                Drawer.PrintLifes(Game.Lifes);
+                Drawer.PrintField();
+                Console.Write("Введите букву: ");
+                string letter = Console.ReadLine();
+                Game.InsertRightLetter(letter);
+            }
+            Drawer.WriteResult();
+        }
+    }
+}
